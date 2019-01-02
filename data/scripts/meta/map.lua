a29ddb2f63b9c17faf0892d72cb9025559b950b6
MaxMraz <Max.Mraz.11@gmail.com>
2019-01-02 00:15:16 -0500

    misc tile errors, graphic tweaks, enemy balancing, quest dialog copy edits, mapping changes

--- data/scripts/meta/map.lua
+++ data/scripts/meta/map.lua
@@ -9,19 +9,36 @@ map_meta:register_event("on_started", function(self)
 	local map = self
   local hero = map:get_hero()
 
+  --make invisible stairs invisible
   for stairs in map:get_entities("^invisible_stairs") do
     stairs:set_visible(false)
   end
 
+  --universal layer up sensors move you up a layer
   for sensor in map:get_entities("^layer_up_sensor") do
     function sensor:on_activated()
       hero:set_layer(hero:get_layer() + 1)
     end
   end
 
+  --and down a layer
   for sensor in map:get_entities("^layer_down_sensor") do
     function sensor:on_activated()
       hero:set_layer(hero:get_layer() - 1)
+    end
+  end
+
+  --generic sensor to save solid ground
+  for sensor in map:get_entities("^save_solid_ground_sensor") do
+    function sensor:on_activated()
+      hero:save_solid_ground()
+    end
+  end
+
+  --generic sensor to reset solid ground.
+  for sensor in map:get_entities("^reset_solid_ground_sensor") do
+    function sensor:on_activated()
+      hero:reset_solid_ground()
     end
   end
 
