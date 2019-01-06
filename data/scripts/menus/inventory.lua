6224adf8007dff84b5b86dd9d0a003eace4492d4
MaxMraz <Max.Mraz.11@gmail.com>
2019-01-06 15:44:43 -0500

    move all inventory items into one category

--- data/scripts/menus/inventory.lua
+++ data/scripts/menus/inventory.lua
@@ -5,9 +5,7 @@ local all_equipment_items = {
     "bow",
     "bow_fire",
     "bombs_counter_2",
-    "boomerang"
-}
-local all_menu_items = {
+    "boomerang",
     "menu_berry"
 }
 --constants:
@@ -15,8 +13,6 @@ local GRID_ORIGIN_X = 10
 local GRID_ORIGIN_Y = 72
 local GRID_ORIGIN_EQUIP_X = GRID_ORIGIN_X
 local GRID_ORIGIN_EQUIP_Y = GRID_ORIGIN_Y
-local GRID_ORIGIN_MENU_ITEM_X = GRID_ORIGIN_X
-local GRID_ORIGIN_MENU_ITEM_Y = GRID_ORIGIN_Y + 32
 local ROWS = 3
 local COLUMNS = 4
 
@@ -27,7 +23,6 @@ function inventory:initialize(game)
 --    local game = sol.main.game
     cursor_index = game:get_value("inventory_cursor_index") or 1
     self.equipment_sprites = {}
-    self.menu_sprites = {}
 
     --create the item sprites:
     for i=1, #all_equipment_items do
@@ -39,18 +34,6 @@ function inventory:initialize(game)
                 self.equipment_sprites[i]:set_animation(all_equipment_items[i])
 --                self.equipment_sprites[i]:set_direction(variant - 1)
                 self.equipment_sprites[i]:set_direction(0)
-            end
-        end
-    end
-    for i=1, #all_menu_items do
-        if all_menu_items[i] ~= "" then
-            local item = game:get_item(all_menu_items[i])
-            local variant = item:get_variant()
-            if variant > 0 then
-                self.menu_sprites[i] = sol.sprite.create("entities/items")
-                self.menu_sprites[i]:set_animation(all_menu_items[i])
-                -- self.menu_sprites[i]:set_direction(variant - 1)
-                self.menu_sprites[i]:set_direction(0)
             end
         end
     end
@@ -78,13 +61,6 @@ function inventory:on_draw(dst_surface)
         end
     end
 
-    --draw menu items
-    for i=1, #all_menu_items do
-        if self.menu_sprites[i] then
-            self.menu_sprites[i]:draw(self.menu_background, GRID_ORIGIN_MENU_ITEM_X + (i*32), GRID_ORIGIN_MENU_ITEM_Y)
-
-        end
-    end
 end
 
 
