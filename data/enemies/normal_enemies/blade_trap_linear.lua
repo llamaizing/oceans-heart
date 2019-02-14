1e05a134530ccef68a146e6f0a8a6b05bab23fa7
MaxMraz <Max.Mraz.11@gmail.com>
2019-02-13 21:18:00 -0500

    move most entities based on tileset to normal entity graphic

---
+++ data/enemies/normal_enemies/blade_trap_linear.lua
@@ -0,0 +1,39 @@
+local enemy = ...
+local game = enemy:get_game()
+local map = enemy:get_map()
+local direction
+
+function enemy:on_created()
+
+  sprite = enemy:create_sprite("enemies/normal_enemies/blade_trap")
+  enemy:set_life(1)
+  enemy:set_damage(2)
+  if enemy:get_property("damage") then enemy:set_damage(enemy:get_property("damage")) end
+  enemy:set_invincible(true)
+  direction = 0
+  if enemy:get_property("direction") and enemy:get_property("direction") == "vertical" then direction = math.pi/2 end
+end
+
+local function go()
+  local m = sol.movement.create("straight")
+  m:set_max_distance(0) --infinite max distance
+  m:set_angle(direction)
+  m:set_smooth(false)
+  --Speed
+  if not enemy:get_property("speed") then
+    m:set_speed(130)
+  else
+    m:set_speed(enemy:get_property("speed"))
+  end
+  m:start(enemy)
+
+  function m:on_obstacle_reached()
+    sol.audio.play_sound("sword_tapping")
+    direction = direction + math.pi
+    go()    
+  end
+end
+
+function enemy:on_restarted()
+  go()
+end
--- data/items/ball_and_chain_2.lua
+++ data/items/ball_and_chain_2.lua
@@ -1,15 +1,6 @@
 local item = ...
 local game = item:get_game()
 
-local NUM_LINKS = 7
-local MIN_RADIUS = 2
-local RADIUS = 48
-local RAIUS_SPEED = 250
-local MAX_ROTATIONS = 3
-local ANGULAR_SPEED = 13
-local CHARGING_TIME = 500
-
-
 -- Event called when the game is initialized.
 function item:on_started()
   item:set_savegame_variable("possession_ball_and_chain")
@@ -33,66 +24,60 @@ function item:on_using()
   hero:start_state(summoning_state)
 
   local x, y, layer = hero:get_position()
-  local links = {}
-  local link_movements = {}
+  local MIN_RADIUS = 2
+  local RADIUS = 56
+  local RAIUS_SPEED = 250
+  local MAX_ROTATIONS = 3
+  local ANGULAR_SPEED = 13
+  local CHARGING_TIME = 500
   
   --now move x or y depending on hero facing direction
   local start_x = x
   local start_y = y
   if hero_dir == 0 then start_x = x - 16 elseif hero_dir == 1 then start_y = y + 16 elseif hero_dir == 2 then start_x = x + 16 elseif hero_dir == 3 then start_y = y - 16 end
 
-  local flail_x = x
-  local flail_y = y
-  local start_angle = 0
-  if hero_dir == 0 then flail_x = x + 16 start_angle = 0
-  elseif hero_dir == 1 then
-    flail_y = y - 16
-    start_angle = math.pi / 2
-  elseif hero_dir == 2 then
-    flail_x = x - 16
-    start_angle = math.pi
-  elseif hero_dir == 3 then
-    flail_y = y + 16
-    start_angle = 3 * math.pi / 2
-  end
-
   --create chain links
-  for i=1, NUM_LINKS do
-    links[i] = map:create_custom_entity{
-      name = "flail_chain_link",
-      direction = 0,
-      layer = layer,
-      x = start_x,
-      y = start_y,
-      width = 8,
-      height = 8,
-      sprite = "entities/chain_link",
-      model = "damaging_sparkle"
-    }
-    link_movements[i] = sol.movement.create("circle")
-    link_movements[i]:set_center(flail_x, flail_y)
-    link_movements[i]:set_radius(MIN_RADIUS)
-    link_movements[i]:set_radius_speed(RAIUS_SPEED)
-    link_movements[i]:set_max_rotations(MAX_ROTATIONS)
-    link_movements[i]:set_angular_speed(ANGULAR_SPEED)
-    if hero_dir == 0 or hero_dir == 3 then link_movements[i]:set_clockwise() end
-
-  end
-
-  --create the spike ball
-  local spike_ball = map:create_custom_entity{
-    name = "flail_spike_ball",
+  local link1 = map:create_custom_entity{
     direction = 0,
     layer = layer,
     x = start_x,
     y = start_y,
     width = 8,
     height = 8,
+    sprite = "entities/chain_link",
+    model = "chain_link"
+  }
+  local link2 = map:create_custom_entity{
+    direction = 0,
+    layer = layer,
+    x = start_x,
+    y = start_y,
+    width = 8,
+    height = 8,
+    sprite = "entities/chain_link",
+    model = "chain_link"
+  }
+  --create the spike ball
+  local spike_ball = map:create_custom_entity{
+    name = "spike_ball",
+    direction = 0,
+    layer = layer,
+    x = start_x,
+    y = start_y,
+    width = 16,
+    height = 16,
     sprite = "entities/spike_ball",
     model = "damaging_sparkle"
   }
   spike_ball:set_damage(game:get_value("sword_damage") + game:get_value("sword_damage")/2)
 
+  local flail_x = x
+  local flail_y = y
+  local start_angle = 0
+  if hero_dir == 0 then flail_x = x + 16 start_angle = 0
+  elseif hero_dir == 1 then flail_y = y - 16 start_angle = math.pi / 2
+  elseif hero_dir == 2 then flail_x = x - 16 start_angle = math.pi
+  elseif hero_dir == 3 then flail_y = y + 16 end start_angle = 3 * math.pi / 2
 
   --create a movement for the flail
   local m = sol.movement.create("circle")
@@ -104,6 +89,23 @@ function item:on_using()
   m:set_angular_speed(ANGULAR_SPEED)
   if hero_dir == 0 or hero_dir == 3 then m:set_clockwise() end
 
+  local m2 = sol.movement.create("circle")
+  m2:set_center(flail_x, flail_y)
+--  m:set_angle_from_center(start_angle)
+  m2:set_radius(MIN_RADIUS)
+  m2:set_radius_speed(RAIUS_SPEED)
+  m2:set_max_rotations(MAX_ROTATIONS)
+  m2:set_angular_speed(ANGULAR_SPEED)
+  if hero_dir == 0 or hero_dir == 3 then m2:set_clockwise() end
+
+  local m3 = sol.movement.create("circle")
+  m3:set_center(flail_x, flail_y)
+--  m:set_angle_from_center(start_angle)
+  m3:set_radius(MIN_RADIUS)
+  m3:set_radius_speed(RAIUS_SPEED)
+  m3:set_max_rotations(MAX_ROTATIONS)
+  m3:set_angular_speed(ANGULAR_SPEED)
+  if hero_dir == 0 or hero_dir == 3 then m3:set_clockwise() end
 
 
   --START CHARGING (because this is too powerful to not charge)
@@ -114,11 +116,11 @@ function item:on_using()
     hero:set_animation("hookshot")
     sol.audio.play_sound("boomerang")
     m:start(spike_ball, function() spike_ball:remove() hero:unfreeze() end)
-    for i=1, NUM_LINKS do
-      link_movements[i]:start(links[i], function() links[i]:remove() end)
-      link_movements[i]:set_radius(RADIUS / NUM_LINKS * i)
-    end
+    m2:start(link1, function() link1:remove() end)
+    m3:start(link2, function() link2:remove() end)
     m:set_radius(RADIUS)
+    m2:set_radius(RADIUS / 3 * 2)
+    m3:set_radius(RADIUS / 3)
   end)
 
 
@@ -126,20 +128,18 @@ function item:on_using()
   function m:on_finished()
     hero:unfreeze()
     spike_ball:remove()
-    for i=1, NUM_LINKS do links[i]:remove() end
+    link1:remove()
+    link2:remove()
     item:set_finished()
   end
 
   --if the movement collides with something
-  local can_play_sound = true
   function m:on_obstacle_reached()
-    if can_play_sound then
-      sol.audio.play_sound("sword_tapping")
-      can_play_sound = false
-      sol.timer.start(game, 500, function() can_play_sound = true end)
-    end
+    spike_ball:get_sprite():set_animation("sparking")
+    sol.audio.play_sound("sword_tapping")
 
-    --to cause a fucking explosion if the variant is 2 or more
+    --these commented out lines would cause an explosion if the flail contacts something.
+    --I'm saving them because that's kinda sick.
     if item:get_variant() >= 2 then
       local spike_x, spike_y, spike_layer = spike_ball:get_position()
       map:create_explosion{layer = spike_layer, x = spike_x, y = spike_y}
