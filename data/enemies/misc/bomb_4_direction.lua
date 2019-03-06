26ae6d088f514ff08b94f8bf627f56da07575d10
MaxMraz <Max.Mraz.11@gmail.com>
2019-03-06 15:34:44 -0500

    beef up dream cannon hideout, add pirate gunner enemy

---
+++ data/enemies/misc/bomb_4_direction.lua
@@ -0,0 +1,51 @@
+-- Bomb thrown by arborgeist bombers.
+
+local enemy = ...
+local map = enemy:get_map()
+
+function enemy:on_created()
+
+  enemy:set_life(1)
+  enemy:set_damage(4)
+  enemy:create_sprite("enemies/misc/thyme_bomb")
+  enemy:set_size(8, 8)
+  enemy:set_origin(4, 4)
+  enemy:set_invincible()
+  enemy:set_obstacle_behavior("flying")
+  enemy:set_attack_consequence("sword", "custom")
+  enemy:set_attack_consequence("arrow", "ignored")
+end
+
+function enemy:on_obstacle_reached()
+  local bombx, bomby, bomblayer = enemy:get_position()
+  sol.audio.play_sound("explosion")
+  map:create_explosion({
+    layer = bomblayer,
+    x = bombx,
+    y = bomby,
+  })
+  enemy:remove()
+end
+
+function enemy:go(direction)
+  local directions = {[0]=0,[1]=math.pi/2,[2]=math.pi,[3]=3*math.pi/2}
+  direction = directions[direction]
+  local movement = sol.movement.create("straight")
+  movement:set_speed(150)
+  movement:set_angle(direction)
+  movement:set_smooth(false)
+  movement:start(enemy)
+end
+
+
+function enemy:on_hurt()
+  sol.audio.play_sound("explosion")
+  local bombx, bomby, bomblayer = enemy:get_position()
+  map:create_explosion({
+    layer = bomblayer,
+    x = bombx,
+    y = bomby,
+  })
+  enemy:remove()
+end
+--]]
---
+++ data/enemies/normal_enemies/pirate_gunner.lua
@@ -0,0 +1,50 @@
+local enemy = ...
+
+local properties_setter = require("enemies/lib/properties_setter")
+local behavior = require("enemies/lib/general_enemy")
+
+local properties = {
+  sprite = "enemies/" .. enemy:get_breed(),
+  life = 10,
+  damage = 4,
+  normal_speed = 5,
+  faster_speed = 15,
+  detection_distance = 140,
+  attack_distance = 55,
+  wind_up_time = 500,
+  attack_sound = "sword2",
+  must_be_aligned_to_attack = false,
+--  push_hero_on_sword = true,
+  must_be_aligned_to_attack = true,
+
+
+  has_ranged_attack = true,
+  ranged_attack_distance = 190,
+  ranged_attack_cooldown = 3000,
+  ranged_attack_sound = "shoot",
+  projectile_breed = "misc/bomb_4_direction",
+  projectile_angle = "straight",
+
+--for circleing hero movement:
+  movement_circle_hero = true,
+  movement_circle_hero_radius = 54,
+    movement_circle_hero_radius_speed = 10,
+}
+
+properties_setter:set_properties(enemy, properties)
+behavior:create(enemy, properties)
+
+function enemy:on_dead()
+  random = math.random(100)
+  if random < 35 then
+    local map = enemy:get_map()
+    local x, y, layer = enemy:get_position()
+    map:create_pickable{
+     layer = layer,
+     x = x,
+     y = y,
+     treasure_name = "rupee",
+     treasure_variant = 2,
+     }
+  end
+end
