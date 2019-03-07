199e87a0b15e3d037bd1e71c1822e9e97771c6f4
MaxMraz <Max.Mraz.11@gmail.com>
2019-03-07 14:30:02 -0500

    give barbell brute captain a vacation

--- data/maps/oakhaven/eastoak.lua
+++ data/maps/oakhaven/eastoak.lua
@@ -17,6 +17,8 @@ map:register_event("on_started", function()
   if game:get_value("fort_crow_front_door_open") == true then map:set_doors_open("front_door") end
   if game:get_value("thyme_defeated") == true then guard_2:set_enabled(false) boat:set_enabled(false) boat_2:set_enabled(false) end
 
+  if game:get_value("barbell_brutes_defeated") == true then vice_captain:set_enabled(true) end
+
   local m1 = sol.movement.create("path")
   m1:set_path{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,6,6,6,6,6,6,6,6,6,6,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
     2,2,2,2,2,2,2,2,2,2,2}
@@ -25,3 +27,8 @@ map:register_event("on_started", function()
   m1:start(circulate_guy)
   circulate_guy:set_traversable(true)
 end)
+
+
+function vice_captain:on_interaction()
+  game:start_dialog("_oakhaven.npcs.vice_captain.1")
+end
