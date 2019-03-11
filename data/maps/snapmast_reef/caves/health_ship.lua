51903ee57b9ba47412df691786ff741ef1f9d0ba
MaxMraz <Max.Mraz.11@gmail.com>
2019-03-10 23:55:33 -0400

    add health upgrade in snapmast, also realize warp bolts are broken

---
+++ data/maps/snapmast_reef/caves/health_ship.lua
@@ -0,0 +1,27 @@
+-- Lua script of map snapmast_reef/caves/health_ship.
+-- This script is executed every time the hero enters this map.
+
+-- Feel free to modify the code below.
+-- You can add more events and remove the ones you don't need.
+
+-- See the Solarus Lua API documentation:
+-- http://www.solarus-games.org/doc/latest
+
+local map = ...
+local game = map:get_game()
+
+-- Event called at initialization time, as soon as this map is loaded.
+map:register_event("on_started", function()
+
+end
+
+
+for enemy in map:get_entities("enemy") do
+  function enemy:on_dead()
+    if not map:has_entities("enemy") and not game:get_value("snapmast_health_ship_upgrade") then
+      map:create_pickable{
+        name="health_upgrade",
+        x = 256, y = 120, layer = 0, treasure_savegame_variable = "snapmast_health_ship_upgrade"}
+    end
+  end
+end
