6d80956269f07ab38628d9663d3f8244a1e39a18
maxmraz <max.mraz.11@gmail.com>
2019-04-21 15:49:16 -0400

    change goatshead alchemist to shop

--- data/maps/goatshead_island/goatshead_harbor.lua
+++ data/maps/goatshead_island/goatshead_harbor.lua
@@ -133,22 +133,6 @@ game:start_dialog("_goatshead.npcs.juglan.ride_back", function(answer)
       end)
 end
 
-function market_shop:on_interaction()
-  game:start_dialog("_ballast_harbor.npcs.market_people.shop", function()
-    local shop_menu = require("scripts/shops/shop_menu")
-    shop_menu:initialize(game)
-    sol.menu.start(map, shop_menu)
-  end)
-end
-
-function buyer_guy:on_interaction()
-  game:start_dialog("_generic_dialogs.buyer_guy.1", function()
-    local sell_menu = require("scripts/shops/sell_menu")
-    sell_menu:initialize(game)
-    sol.menu.start(map, sell_menu)
-  end)
-end
-
 
 function upset_fisher:on_interaction()
   if game:get_value("two_eye_rock_shroom_defeated") == nil then
