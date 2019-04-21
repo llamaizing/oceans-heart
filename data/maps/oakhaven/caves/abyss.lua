ec0d8c569eddcf6c32a25bd441855f0d221f21bc
maxmraz <max.mraz.11@gmail.com>
2019-04-20 23:08:06 -0400

    add smoldering rock

--- data/maps/oakhaven/caves/abyss.lua
+++ data/maps/oakhaven/caves/abyss.lua
@@ -9,12 +9,30 @@
 
 local map = ...
 local game = map:get_game()
+local particles = {}
+local MAX_PARTICLES = 50
+local PARTICLE_SPEED = 11
 
 -- Event called at initialization time, as soon as this map is loaded.
 map:register_event("on_started", function()
   if game:get_value("oakhaven_palace_rune_activated") then
     glowing_rune:set_enabled(true)
+    rune_sensor:set_enabled(true)
   end
+
+--particle effect creation
+  local i = 1
+  sol.timer.start(map, math.random(100,250), function()
+    particles[i] = sol.sprite.create("entities/pollution_ash")
+    particles[i]:set_xy(math.random(-350, 300), math.random(-800, 200))
+    local m = sol.movement.create("random")
+    m:set_speed(PARTICLE_SPEED)
+--    m:set_ignore_suspend(false)
+    m:start(particles[i])
+    i = i + 1
+    if i > MAX_PARTICLES then i = 0 end
+    return true
+  end)
 
 end)
 
@@ -24,5 +42,15 @@ function coral_boss:on_dead()
 end
 
 function rune_sensor:on_activated()
+print("hi maxx")
+  game:start_dialog("_oakhaven.observations.abyss.draw_in")
+end
 
+
+--particle effect draw
+function map:on_draw()
+    local x, y, layer = glowing_rune:get_position()
+    for i=1, #particles do
+      map:draw_visual(particles[i], x, y)
+    end
 end
---
+++ data/maps/snapmast_reef/caves/smoldering_rock.lua
@@ -0,0 +1,54 @@
+-- Lua script of map snapmast_reef/caves/smoldering_rock.
+-- This script is executed every time the hero enters this map.
+
+-- Feel free to modify the code below.
+-- You can add more events and remove the ones you don't need.
+
+-- See the Solarus Lua API documentation:
+-- http://www.solarus-games.org/doc/latest
+
+local map = ...
+local game = map:get_game()
+
+-- Event called at initialization time, as soon as this map is loaded.
+map:register_event("on_started", function()
+  local x, y, l = emblem:get_position()
+  game:set_life(game:get_max_life())
+  local zapper = game:get_item("thunder_charm")
+  if not game:get_value("smoldering_rock_seabird_charm_pickable_variable") then
+    map:create_pickable({
+      x = x + 8, y = y + 16, layer = l,
+      treasure_name = "thunder_charm", treasure_variant = zapper:get_variant() + 1,
+      treasure_savegame_variable = "smoldering_rock_seabird_charm_pickable_variable",
+    })
+  end
+
+  if game:get_value("smoldering_rock_found_fire_arrows") then
+    fire_arrow_boss:set_enabled(false)
+  end
+
+end)
+
+
+function ring_switch_a:on_activated()
+  map:open_doors("ring_a_door")
+  sol.audio.play_sound"switch"
+  sol.audio.play_sound"secret"
+  map:get_camera():shake()
+end
+
+function ring_switch_b:on_activated()
+  map:open_doors("ring_b_door")
+  sol.audio.play_sound"switch"
+  sol.audio.play_sound"secret"
+  map:get_camera():shake()
+end
+
+
+function fire_arrow_boss:on_dead()
+  map:open_doors("fire_arrow_door")
+end
+
+function seabird_boss:on_dead()
+  map:open_doors("second_boss_door")
+end
