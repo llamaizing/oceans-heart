87a77316520ca3ef45acaa8e04012db6677c587a
maxmraz <max.mraz.11@gmail.com>
2019-04-25 12:08:04 -0400

    add limestone pit

---
+++ data/maps/new_limestone/caves/pit.lua
@@ -0,0 +1,16 @@
+-- Lua script of map new_limestone/caves/pit.
+-- This script is executed every time the hero enters this map.
+
+-- Feel free to modify the code below.
+-- You can add more events and remove the ones you don't need.
+
+-- See the Solarus Lua API documentation:
+-- http://www.solarus-games.org/doc/latest
+
+local map = ...
+local game = map:get_game()
+
+
+function boss:on_dead()
+  map:open_doors("boss_door")
+end
