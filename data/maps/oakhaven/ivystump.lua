dcc493002f0604625048543a101d6c8770281033
maxmraz <max.mraz.11@gmail.com>
2019-04-29 17:35:44 -0400

    continue to implement ivystump sidequest

--- data/maps/oakhaven/ivystump.lua
+++ data/maps/oakhaven/ivystump.lua
@@ -14,10 +14,17 @@ local game = map:get_game()
 map:register_event("on_started", function()
   require("scripts/fx/sound_atmosphere_manager"):start_atmosphere(map, "birds")
 
+  --ivy orchard quest
+  if game:get_value("quest_ivy_orchard") and game:get_value("quest_ivy_orchard") >= 1 then
+    hole_hider:set_enabled(false)
+    picker_paul:set_enabeld(false)
+    for boss in map:get_entities("apple_boss") do boss:set_enabled(false) end
+  end
+
 end)
 
 
-
+--quest numbers: 0 = clear orchard, 1 = return to paul, 2 = save paul, 3 = done
 function picker_paul:on_interaction()
   if not game:get_value("quest_ivy_orchard") then
     game:start_dialog("_oakhaven.npcs.ivystump.picker_paul.1", function(answer)
@@ -41,8 +48,29 @@ end
 
 function picker_peter:on_interaction()
   if not game:get_value("quest_ivy_orchard") or game:get_value("quest_ivy_orchard") == 0 then
-
+    game:start_dialog("_oakhaven.npcs.ivystump.picker_peter.1")
   elseif game:get_value("quest_ivy_orchard") == 1 then
+    game:start_dialog("_oakhaven.npcs.ivystump.picker_peter.2")
+  end
+end
 
+
+for boss in map:get_entities("apple_boss") do
+boss:on_dead()
+  if not map:has_entities("apple_boss") then
+    sol.audio.play_sound("secret")
+    game:set_value("quest_ivy_orchard", 1)
+    hole_hider:set_enabled(false)
+    picker_paul:set_enabeld(false)
+  end
+end
+end
+
+
+function paul_is_gone_sensor:on_activated()
+  if game:get_value("quest_ivy_orchard") == 1 then
+    game:start_dialog("_oakhaven.npcs.ivystump.picker_peter.2", function()
+      game:set_value("quest_ivy_orchard", 2)
+    end)
   end
 end
