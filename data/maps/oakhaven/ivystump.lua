f6b99bb210182349b694800a3f4595966baa8966
maxmraz <max.mraz.11@gmail.com>
2019-04-29 17:15:56 -0400

    start implementing sidequest in Ivystump

--- data/maps/oakhaven/ivystump.lua
+++ data/maps/oakhaven/ivystump.lua
@@ -15,3 +15,34 @@ map:register_event("on_started", function()
   require("scripts/fx/sound_atmosphere_manager"):start_atmosphere(map, "birds")
 
 end)
+
+
+
+function picker_paul:on_interaction()
+  if not game:get_value("quest_ivy_orchard") then
+    game:start_dialog("_oakhaven.npcs.ivystump.picker_paul.1", function(answer)
+      if answer == 3 then
+        game:start_dialog("_oakhaven.npcs.ivystump.picker_paul.2", function()
+          game:set_value("quest_ivy_orchard", 0)
+          for crab in map:get_entities("apple_boss") do crab:set_enabled() end
+        end)
+      end
+    end)
+
+  elseif game:get_value("quest_ivy_orchard") == 0 then
+    game:start_dialog("_oakhaven.npcs.ivystump.picker_paul.3")
+
+  elseif game:get_value("quest_ivy_orchard") == 1 then
+
+  end
+
+end
+
+
+function picker_peter:on_interaction()
+  if not game:get_value("quest_ivy_orchard") or game:get_value("quest_ivy_orchard") == 0 then
+
+  elseif game:get_value("quest_ivy_orchard") == 1 then
+
+  end
+end
