94ccf8b5e331e446a452a1c3f343ee269d9be1e6
maxmraz <max.mraz.11@gmail.com>
2019-05-10 23:59:28 -0400

    remove 1.5 backup from repo

--- data.1.5.bak/items/honeycomb.lua
+++
@@ -1,8 +0,0 @@
-local item = ...
-local game = item:get_game()
-
-
-function item:on_created()
-  item:set_savegame_variable("possession_honeycomb")
-
-end
--- data.1.5.bak/scripts/menus/respawn_screen.lua
+++
@@ -1,9 +0,0 @@
-respawn_screen = {}
-
-local black_screen = sol.surface.create()
-black_screen:fill_color({0,0,0})
-
-function respawn_screen:on_draw(dst_surface)
-  black_screen:draw(dst_surface)
-
-end
