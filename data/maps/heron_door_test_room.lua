accaca6df55f817678b8b2d2bbfa80e3e917055e
maxmraz <max.mraz.11@gmail.com>
2019-08-18 22:33:58 -0400

    fix var sub in quest dialog

--- data/maps/heron_door_test_room.lua
+++ data/maps/heron_door_test_room.lua
@@ -40,6 +40,7 @@ function monk:on_interaction()
   game:get_item("thunder_charm"):set_variant(1)
   game:set_value("possession_thunder_charm", 1)
 print("I gave you the charm")
-  game:set_value("quest_heron_doors", 1)
+--  game:set_value("quest_heron_doors", 0)
+  game:set_value("heron_doors", 0)
   game.objectives:set_alternate("heron_doors_alt", "quest.heron_doors")
 end
