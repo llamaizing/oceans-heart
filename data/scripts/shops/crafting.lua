900640439487c44406eb8df9523b6581a49f19b8
maxmraz <max.mraz.11@gmail.com>
2019-08-31 10:32:16 -0400

    finish programming crafting menu

--- data/scripts/shops/crafting.lua
+++ data/scripts/shops/crafting.lua
@@ -3,53 +3,52 @@ local multi_events = require"scripts/multi_events"
 local crafting_menu = {x=0, y=0}
 multi_events:enable(crafting_menu)
 
-----------------------------------
-local cursor_index = 0
-
-
 --Recipes List--------------------
 local unlocked_recipes = {}
 
 local all_recipes = {
   {
     item_name = "elixer",
-    ingredients = {"kingscrown", "ghost_orchid", "firethorn_berries"},
+    ingredients = {{"kingscrown", 1}, {"ghost_orchid", 1}, {"firethorn_berries", 3}},
     unlock_variable = "is_craftable_elixer"
   },
   {
     item_name = "potion_magic_restoration",
-    ingredients = {"witch_hazel", "mandrake", "monster_guts"},
+    ingredients = {{"witch_hazel", 2}, {"mandrake", 1}, {"monster_guts", 3}},
     unlock_variable = "is_craftable_potion_magic_restoration"
   },
   {
     item_name = "potion_stoneskin",
-    ingredients = {"mandrake_white", "geode", "monster_bones"},
+    ingredients = {{"mandrake_white", 1}, {"geode", 3}, {"monster_bones", 3}},
     unlock_variable = "is_craftable_potion_stoneskin"
   },
   {
     item_name = "potion_burlyblade",
-    ingredients = {"kingscrown", "burdock", "monster_guts"},
+    ingredients = {{"kingscrown", 1}, {"burdock", 3}, {"monster_guts", 3}},
     unlock_variable = "is_craftable_potion_burlyblade"
   },
   {
     item_name = "potion_fleetseed",
-    ingredients = {"ghost_orchid", "lavendar", "monster_guts"},
+    ingredients = {{"ghost_orchid", 1}, {"lavendar", 3}, {"monster_guts", 3}},
     unlock_variable = "is_craftable_potion_fleetseed"
   },
   {
-    item_name = "ether_bombs_pickable",
-    ingredients = {"mandrake", "lavendar", "chamomile"},
-    unlock_variable = "is_craftable_ether_bombs"
+    item_name = "ether_bombs",
+    ingredients = {{"mandrake", 2}, {"lavendar", 2}, {"chamomile", 2}},
+    unlock_variable = "is_craftable_ether_bombs",
+    amount_created = 5,
   },
   {
-    item_name = "iron_candle_pickable",
-    ingredients = {"monster_bones", "geode", "witch_hazel"},
-    unlock_variable = "is_craftable_salt_candles"
+    item_name = "iron_candle",
+    ingredients = {{"monster_bones", 2}, {"geode", 2}, {"witch_hazel", 2}},
+    unlock_variable = "is_craftable_salt_candles",
+    amount_created = 5,
   },
   {
-    item_name = "homing_eye_pickable",
-    ingredients = {"monster_eye", "burdock", "firethorn_berries"},
-    unlock_variable = "is_craftable_seeker_eyes"
+    item_name = "homing_eye",
+    ingredients = {{"monster_eye", 3}, {"burdock", 2}, {"firethorn_berries", 2}},
+    unlock_variable = "is_craftable_seeker_eyes",
+    amount_created = 5,
   },
 
 }
@@ -57,22 +56,26 @@ local all_recipes = {
 
 
 --Surfaces, etc.------------------
-local menu_surface = sol.surface.create()
---initialize background (basically just the frame)
-local menu_dark_overlay = sol.surface.create("menus/dark_overlay.png")
-menu_dark_overlay:draw(menu_surface)
-local menu_background = sol.surface.create("menus/crafting_background.png")
-menu_background:draw(menu_surface)
---initialize the cursor
-local cursor_sprite = sol.sprite.create("menus/cursor")
-local recipes_surface = sol.surface.create(208, 192)
-local ingredients_surface = sol.surface.create()
+function crafting_menu:initialize()
+  self.cursor_index = 0
+  self.menu_surface = sol.surface.create()
+  --initialize background (basically just the frame)
+  self.menu_dark_overlay = sol.surface.create("menus/dark_overlay.png")
+  self.menu_dark_overlay:draw(self.menu_surface)
+  self.menu_background = sol.surface.create("menus/crafting_background.png")
+  self.menu_background:draw(self.menu_surface)
+  --initialize the cursor
+  self.cursor_sprite = sol.sprite.create("menus/cursor")
+  self.recipes_surface = sol.surface.create(208, 192)
+  self.ingredients_surface = sol.surface.create()
+end
 
 
------------------------------------
+--Menu Functions-----------------
 
 function crafting_menu:on_started()
   sol.main.get_game():set_suspended(true)
+  crafting_menu:initialize()
   crafting_menu:update_recipes()
   crafting_menu:update_ingredients()
 end
@@ -82,7 +85,9 @@ function crafting_menu:on_finished()
 end
 
 function crafting_menu:update_recipes()
+  self.recipes_surface:clear()
   --put all unlocked recipes in unlocked_recipes
+  unlocked_recipes = {}
   local game = sol.main.get_game()
   for i=1, #all_recipes do
     if game:get_value(all_recipes[i].unlock_variable) then
@@ -96,16 +101,19 @@ function crafting_menu:update_recipes()
       font="oceansfont",
       text_key="item."..unlocked_recipes[i].item_name
     }
-    recipe_surface:draw(recipes_surface, 0, 18 * i - 18)
+    if not crafting_menu:can_make_recipe(unlocked_recipes[i]) then
+      recipe_surface:set_color_modulation{150,150,150,200}
+    end
+    recipe_surface:draw(self.recipes_surface, 0, 18 * i - 18)
   end
-  recipes_surface:draw(menu_surface, 208, 56)
+  self.recipes_surface:draw(self.menu_surface, 208, 56)
 end
 
 function crafting_menu:update_ingredients()
   local game = sol.main.get_game()
-  local current_recipe = unlocked_recipes[cursor_index + 1]
+  local current_recipe = unlocked_recipes[self.cursor_index + 1]
   local ingredients = current_recipe.ingredients
-  ingredients_surface:clear()
+  self.ingredients_surface:clear()
   --where to draw the ingredients
   local locations = {
     {x=96, y=82},
@@ -114,10 +122,52 @@ function crafting_menu:update_ingredients()
   }
   for i=1, #ingredients do
     local sprite = sol.sprite.create("entities/items")
-    sprite:set_animation(ingredients[i])
-    sprite:draw(ingredients_surface, locations[i].x, locations[i].y)
+    sprite:set_animation(ingredients[i][1])
+    sprite:draw(self.ingredients_surface, locations[i].x, locations[i].y)
+    local qty_surface = sol.text_surface.create{
+      vertical_alignment="top",
+      font="white_digits",
+      text = game:get_item(ingredients[i][1]):get_amount() .. "/" .. ingredients[i][2]
+    }
+    qty_surface:draw(self.ingredients_surface, locations[i].x - 4, locations[i].y + 18)
   end
 end
+
+
+--Crafting Functions---------------------
+
+function crafting_menu:can_make_recipe(recipe)
+  local can_indeed_make = true
+  local game = sol.main.get_game()
+  for i=1, #recipe.ingredients do
+    local ingredient = recipe.ingredients[i]
+    local item = game:get_item(ingredient[1])
+    if item:get_amount() < ingredient[2] then
+      can_indeed_make = false
+    end
+  end
+  return can_indeed_make
+end
+
+
+function crafting_menu:craft_item(recipe)
+  local game = sol.main.get_game()
+  local item = game:get_item(recipe.item_name)
+  sol.audio.play_sound"treasure"
+  game:start_dialog("_game.crafted_item")
+  if not game:has_item(recipe.item_name) then
+    item:set_variant(1)
+  end
+  item:add_amount(recipe.amount_created or 1)
+  for i=1, #recipe.ingredients do
+    local ingredient = recipe.ingredients[i]
+    local ingredient_item = game:get_item(ingredient[1])
+    ingredient_item:remove_amount(ingredient[2])
+  end
+  crafting_menu:update_ingredients()
+  crafting_menu:update_recipes()
+end
+
 
 
 -----Command Inputs-------------------
@@ -127,17 +177,28 @@ function crafting_menu:on_command_pressed(command)
 
   if command == "down" then
     handled = true
-    cursor_index = cursor_index + 1
-    if cursor_index >= #unlocked_recipes then cursor_index = 0 end
+    self.cursor_index = self.cursor_index + 1
+    if self.cursor_index >= #unlocked_recipes then self.cursor_index = 0 end
     sol.audio.play_sound"cursor"
     crafting_menu:update_ingredients()
 
   elseif command == "up" then
     handled = true
-    cursor_index = cursor_index - 1
-    if cursor_index < 0 then cursor_index = #unlocked_recipes - 1 end
+    self.cursor_index = self.cursor_index - 1
+    if self.cursor_index < 0 then self.cursor_index = #unlocked_recipes - 1 end
     sol.audio.play_sound"cursor"
     crafting_menu:update_ingredients()
+
+  elseif command == "action" then
+    handled = true
+    local current_recipe = unlocked_recipes[self.cursor_index + 1]
+    if crafting_menu:can_make_recipe(current_recipe) then
+      crafting_menu:craft_item(current_recipe)
+    else
+      sol.audio.play_sound"no"
+      game:get_map():get_camera():shake({count=3,amplitude=2})
+    end
+
 
   elseif command == "attack" then
     handled = true
@@ -165,9 +226,9 @@ end
 
 
 function crafting_menu:on_draw(dst)
-  menu_surface:draw(dst)
-  ingredients_surface:draw(dst)
-  cursor_sprite:draw(dst, 200, 60 + cursor_index * 18)
+  self.menu_surface:draw(dst)
+  self.ingredients_surface:draw(dst)
+  self.cursor_sprite:draw(dst, 200, 60 + self.cursor_index * 18)
 end
 
 
