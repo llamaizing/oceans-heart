f108091b863699ba37c10282337910c4e3e2c790
llamazing <llamazing@zoho.com>
2019-09-25 18:59:57 -0500

    fix certain equipment items in hud consumables

--- data/scripts/meta/item.lua
+++ data/scripts/meta/item.lua
@@ -2,14 +2,22 @@
 
 require"scripts/multi_events"
 
---list of consumables
+--list of consumables for consumables hud script
 local ITEM_LIST = {
-  arrow = true,
-  bomb = "bombs_counter.1",
-  iron_candle_pickable = true,
-  ether_bombs_pickable = true,
-  homing_eye_pickable = true,
-  berries = "berries.1",
+  --equipment items
+  bow = true, --item
+  arrow = "bow", --pickable
+  bombs_counter_2 = {}, --item
+  bomb = "bombs_counter_2", --pickable
+  bow_bombs = true,
+  bow_fire = true,
+  iron_candle = true, --item
+  iron_candle_pickable = "iron_candle", --pickable
+  ether_bombs = true, --item
+  ether_bombs_pickable = "ether_bombs", --pickable
+  homing_eye = true, --item
+  homing_eye_pickable = "homing_eye", --pickable
+  berries = "berries.1", --use first variant sprite only
   apples = "apples.1",
   bread = "bread.1",
   elixer = true,
@@ -17,6 +25,7 @@ local ITEM_LIST = {
   potion_fleetseed = true,
   potion_magic_restoration = true,
   potion_stoneskin = true,
+  
   --collectables
   coral_ore = true,
   burdock = true,
@@ -37,20 +46,41 @@ local ITEM_LIST = {
 
 local item_meta = sol.main.get_metatable"item"
 item_meta:register_event("on_obtained", function(self, variant)
-  --trigger quest log update when an item is obtained
+  local game = self:get_game()
+  
+  --## trigger quest log update when an item is obtained
+  
   local savegame_variable = self:get_savegame_variable()
   if savegame_variable then
-    local game = self:get_game()
     game.objectives:refresh(savegame_variable)
   end
 
-  --display hud panel for consumables when obtained
-  local name = self:get_name()
-  if ITEM_LIST[name] and self:has_amount() then
-    local game = self:get_game()
+  --## display hud panel for consumables when obtained
+  
+  local name = self:get_name() --name of the obtained item
+  local item_id = ITEM_LIST[name] --name of the item to display
+  local variant
+  
+  --may substitute for different item
+  if item_id then
+    if type(item_id)=="string" then
+      item_id, variant = item_id:match"^([^%.]+)%.?(%d*)$"
+      assert(item_id, "ITEM_LIST invalid value for key "..name)
+      variant = tonumber(variant) --will be nil of not specified
+    else --item_id has value of true
+      item_id = name --use itself as the item id
+      variant = self:get_variant()
+    end
+  end
+  
+  local item = game:get_item(item_id) --item to be displayed (not necessarily the one obtained)
+  assert(item, "Invalid item specified in ITEM_LIST: "..item_id)
+  local variant = variant or item:get_variant() --use current variant if not specified
+  
+  if item:has_amount() and variant>0 then
     local hud = game:get_hud() or {}
     local menu = hud.elements and hud.elements.consumables
-    if menu then menu:add_item(self, variant) end
+    if menu then menu:add_item(item, variant) end --display a panel for the item on the hud
   end
 end)
 
