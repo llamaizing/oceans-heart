2279f6de493396b7364034353bc5b2588bb122fc
llamazing <llamazing@zoho.com>
2019-09-24 22:57:54 -0500

    preliminary hud menu for consumables

---
+++ data/scripts/hud/consumables.lua
@@ -0,0 +1,188 @@
+--[[consumables.lua
+	version 0.1a1
+	24 Sep 2019
+	GNU General Public License Version 3
+	author: Llamazing
+
+	   __   __   __   __  _____   _____________  _______
+	  / /  / /  /  | /  |/  /  | /__   /_  _/  |/ / ___/
+	 / /__/ /__/ & |/ , ,  / & | ,:',:'_/ // /|  / /, /
+	/____/____/_/|_/_/|/|_/_/|_|_____/____/_/ |_/____/
+
+	This hud script displays a brief popup displaying an item icon and amount whenever the
+	player obtains a consumable item. Multiple popups will be stacked when the player gets
+	multiple items in quick succession.
+]]
+
+require("scripts/multi_events")
+
+local hud_submenu = {}
+
+local MAX_COUNT = 5
+local Y_OFFSET = 28
+local IMG_ID = "menus/panel.png"
+local ITEM_LIST = {
+	--collectables
+	coral_ore = true,
+	burdock = true,
+	chamomile = true,
+	firethorn_berries = true,
+	ghost_orchid = true,
+	kingscrown = true,
+	lavendar = true,
+	witch_hazel = true,
+	mandrake_white = true,
+	mandrake = true,
+	geode = true,
+	monster_bones = true,
+	monster_eye = true,
+	monster_guts = true,
+	monster_heart = true,
+}
+
+	--properties (table, key/value)
+		--x
+		--y
+		--duration
+function hud_submenu:new(game, properties)
+	local menu = {}
+	local panels = {} --circular buffer
+	local panel_index = 1
+	local panel_count = 0
+	
+	local queue = {} --additional panels exceeding max, show when new slots free up
+	local img = sol.surface.create(IMG_ID)
+	
+	--TODO validation
+	local dst_x = properties.x
+	local dst_y = properties.y
+	local duration = properties.duration
+	local is_enabled = true
+	
+	local function remove_panel()
+		local new_panel = table.remove(queue)
+		panels[panel_index] = new_panel --may be nil
+		if not new_panel then panel_count = panel_count - 1 end
+		
+		panel_index = panel_index + 1
+		if panel_index > MAX_COUNT then panel_index = panel_index - MAX_COUNT end
+		
+		--TODO vertical slide translation of panels
+	end
+	
+	function menu:get_dst() return dst_x, dst_y end
+	function menu:set_dst(x,y)
+		x = tonumber(x)
+		y = tonumber(y)
+		assert(type(x)=="number", "Bad argument #2 to 'set_dst' (number expected)")
+		assert(type(y)=="number", "Bad argument #3 to 'set_dst' (number expected)")
+		x = math.floor(x)
+		y = math.floor(y)
+		
+		dst_x = x
+		dst_y = y
+	end
+	
+	function menu:get_enabled() return is_enabled end
+	function menu:set_enabled(enabled)
+		assert(type(enabled)=="boolean", "Bad argument #2 to 'set_enabled' (boolean expected)")
+		is_enabled = enabled
+	end
+	
+	--// Create a new surface to be displayed for the given item and variant
+	function menu:add_item(item, variant)
+		local name = item:get_name()
+		local amount = item:get_amount()
+		local max_amount = item:get_max_amount()
+		print("add:", name, amount, variant)
+		
+		--create item icon sprite
+		local sprite = sol.sprite.create"entities/items"
+		sprite:set_animation(name)
+		local direction = variant - 1
+		sprite:set_direction(direction)
+		
+		--create amount text surface
+		local font = "white_digits"
+		if amount >= max_amount then font = "green_digits" end
+		local text_surface = sol.text_surface.create{
+			horizontal_alignment = "right",
+			vertical_alignment = "top",
+			text = amount,
+			font = font,
+		}
+		
+		local panel = {
+			name = name,
+			variant = variant,
+			amount = amount,
+			max_amount = max_amount,
+			sprite = sprite,
+			text_surface = text_surface,
+			x=0, y=0,
+		}
+		
+		if panel_count < MAX_COUNT then
+			--add new panel
+			local new_index = panel_index + panel_count
+			if new_index > MAX_COUNT then new_index = new_index - MAX_COUNT end
+			panels[new_index] = panel
+			panel_count = panel_count + 1
+			
+			--TODO horizontal slide translation of newly added panel
+			
+			--create timer to remove panel
+			sol.timer.start(self, duration, remove_panel)
+		else table.insert(queue, panel) end --add panel to queue
+	end
+	
+	function menu:on_started()
+		panels = {}
+		panel_index = 1
+		panel_count = 0
+		queue = {}
+	end
+	
+	function menu:on_paused() is_enabled = false end
+	function menu:on_unpaused() is_enabled = true end
+	
+	function menu:on_draw(dst_surface)
+		if not is_enabled then return end
+		
+		local width, height = dst_surface:get_size()
+		local x = dst_x + (dst_x < 0 and width or 0)
+		local y = dst_y + (dst_y < 0 and height or 0)
+		
+		local offset = 0
+		for n=1,panel_count do
+			local i = n + panel_index - 1
+			if i < 1 then i = i + MAX_COUNT end
+			local panel = panels[i]
+			if not panel then break end
+			
+			img:draw(dst_surface, x+panel.x, y+offset+panel.y)
+			local origin_x, origin_y = panel.sprite:get_origin()
+			panel.sprite:draw(dst_surface, x+4+panel.x+origin_x, y+offset+4+panel.y+origin_y)
+			panel.text_surface:draw(dst_surface, x+44+panel.x, y+offset+8+panel.y)
+			offset = offset + Y_OFFSET
+		end
+	end
+	
+	return menu
+end
+
+return hud_submenu
+
+--[[ Copyright 2019 Llamazing
+  [] 
+  [] This program is free software: you can redistribute it and/or modify it under the
+  [] terms of the GNU General Public License as published by the Free Software Foundation,
+  [] either version 3 of the License, or (at your option) any later version.
+  [] 
+  [] It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  [] without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  [] PURPOSE.  See the GNU General Public License for more details.
+  [] 
+  [] You should have received a copy of the GNU General Public License along with this
+  [] program.  If not, see <http://www.gnu.org/licenses/>.
+  ]]
--- data/scripts/meta/item.lua
+++ data/scripts/meta/item.lua
@@ -2,14 +2,43 @@
 
 require"scripts/multi_events"
 
-local item_meta = sol.main.get_metatable"item"
+--list of consumables
+local ITEM_LIST = {
+	--collectables
+	coral_ore = true,
+	burdock = true,
+	chamomile = true,
+	firethorn_berries = true,
+	ghost_orchid = true,
+	kingscrown = true,
+	lavendar = true,
+	witch_hazel = true,
+	mandrake_white = true,
+	mandrake = true,
+	geode = true,
+	monster_bones = true,
+	monster_eye = true,
+	monster_guts = true,
+	monster_heart = true,
+}
 
---trigger quest log update when an item is obtained
-item_meta:register_event("on_obtained", function(self)
+local item_meta = sol.main.get_metatable"item"
+item_meta:register_event("on_obtained", function(self, variant)
+  --trigger quest log update when an item is obtained
   local savegame_variable = self:get_savegame_variable()
   if savegame_variable then
     local game = self:get_game()
     game.objectives:refresh(savegame_variable)
+  end
+
+  --display hud panel for consumables when obtained
+  local name = self:get_name()
+  print(name, variant)
+  if ITEM_LIST[name] and self:has_amount() then
+    local game = self:get_game()
+    local hud = game:get_hud() or {}
+    local menu = hud.elements and hud.elements.consumables
+    if menu then menu:add_item(self, variant) end
   end
 end)
 
