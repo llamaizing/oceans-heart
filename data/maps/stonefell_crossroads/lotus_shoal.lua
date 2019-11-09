d7811375bee8ad98791271cd1bbf6352cf19b345
maxmraz <max.mraz.11@gmail.com>
2019-11-08 19:44:03 -0500

    continue lotus shoal

--- data/maps/stonefell_crossroads/lotus_shoal.lua
+++ data/maps/stonefell_crossroads/lotus_shoal.lua
@@ -10,15 +10,32 @@
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
+map:register_event("on_started", function()
+  if game:get_value("boomerang_fort_boss_killed") then
+    boomerang_boss:set_enabled(false)
+    map:set_doors_open("fort_boss_door")
+  end
+--  bait_monster:set_enabled(false)
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+end)
+
+
+----Fort------
+function fort_switch_1:on_activated()
+  sol.audio.play_sound("switch")
+  map:focus_on(map:get_camera(), fort_door_1, function()
+    map:open_doors("fort_door_1")
+  end)
 end
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
+function fort_switch_2:on_activated()
+  sol.audio.play_sound("switch")
+  map:focus_on(map:get_camera(), fort_door_2, function()
+    map:open_doors("fort_door_2")
+  end)
+end
 
+function boomerang_boss:on_dead()
+  game:set_value("boomerang_fort_boss_killed", true)
+  map:open_doors("fort_boss_door")
 end
