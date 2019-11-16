5add79d8b7cda640efad87935345bbd4c077bbfe
maxmraz <max.mraz.11@gmail.com>
2019-11-16 10:20:31 -0500

    finish Zephyr Bay

--- data/maps/stonefell_crossroads/zephyr_bay.lua
+++ data/maps/stonefell_crossroads/zephyr_bay.lua
@@ -10,15 +10,28 @@
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
+-- Event called at initialization time, as soon as this map becomes is loaded.
+map:register_event("on_started", function()
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
-end
+  if not game:get_value("yarrowmouth_lighthouse_activated") then
+    light_1:set_enabled(false)
+    light_2:set_enabled(false)
+    light_3:set_enabled(false)
+  end
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
+  if game:get_value("quest_lighthouses") and game:get_value("quest_lighthouses") >= 1 and game:get_value("quest_lighthouses") < 3 then
+    fog:set_enabled(true)
+    ghost_ship:set_enabled(true)
+    ghost_ship_sensor:set_enabled(true)
+    sol.audio.stop_music()
+  end
+end)
 
+--ghost ship
+function ghost_ship_sensor:on_activated()
+  game:start_dialog("_yarrowmouth.ghost_ship.board_question", function(answer)
+    if answer == 2 then
+      hero:teleport("Yarrowmouth/caves/ghost_ship", "from_outside")
+    end
+  end)
 end
