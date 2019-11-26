f2c533ca428cebfb703b4a0fea0c91acc5f1c386
maxmraz <max.mraz.11@gmail.com>
2019-11-26 18:26:02 -0500

    start yarrowmouth

---
+++ data/maps/Yarrowmouth/tern_marshOLD.lua
@@ -0,0 +1,99 @@
+-- Lua script of map Yarrowmouth/kingsdown.
+-- This script is executed every time the hero enters this map.
+
+-- Feel free to modify the code below.
+-- You can add more events and remove the ones you don't need.
+
+-- See the Solarus Lua API documentation:
+-- http://www.solarus-games.org/doc/latest
+
+local map = ...
+local game = map:get_game()
+
+-- Event called at initialization time, as soon as this map becomes is loaded.
+map:register_event("on_started", function()
+  if game:get_value("kingsdown_island_unlocked") == true then
+    block_statue_1:set_enabled(false)
+    block_statue_2:set_enabled(false)
+  end
+
+  if game:get_value("yarrow_village_pirate_guard_left") == true then pirate_guard:set_enabled(false) end
+
+  if game:get_value("nina_dialog_counter") ~= 2 then nina:set_enabled(false) end
+
+end)
+
+
+function secret_keyhole:on_interaction()
+  if game:has_item("key_kingsdown") == true and game:get_value("kingsdown_island_unlocked") ~= true then
+    game:start_dialog("_yarrowmouth.observations.secret_switch.2", function(answer)
+      if answer == 3 then
+        local x, y, l = block_statue_1:get_position()
+        map:create_poof(x + 8, y + 16, l + 1)
+        block_statue_1:set_enabled(false)
+        block_statue_2:set_enabled(false)
+        sol.audio.play_sound("switch")
+        sol.audio.play_sound("secret")
+          game:set_value("kingsdown_island_unlocked", true)
+          game:set_value("quest_log_a", "a7")
+          game:set_value("quest_log_b", 0)
+          if game:get_value("quest_hourglass_fort") == 1 then
+            game:set_value("quest_hourglass_fort", 2) --quest log
+          end
+          game:set_value("quest_kelpton", 4) -- quest log
+
+      end
+    end) --end of answer function
+
+  else --don't have the key yet
+    game:start_dialog("_yarrowmouth.observations.secret_switch.1")
+
+  end
+end
+
+function hourglass_bridge_switch:on_activated()
+  map:open_doors("hourglass_door")
+end
+
+function door_item_sensor:on_activated()
+  if not game:has_item("heron_door_tern_marsh") then
+    game:get_item("heron_door_tern_marsh"):set_variant(1)
+    game:set_value("found_heron_door_tern_marsh", 1) --TODO quest log issue #76
+    game.objectives:force_update() --TODO quest log issue #70
+  end
+end
+
+
+
+function pirate_guard:on_interaction()
+  if game:get_value("going_to_meet_carlov_pirates") == true then
+    game:start_dialog("_yarrowmouth.npcs.guard_pirate.2")
+    local m = sol.movement.create("path")
+    m:set_path{2,2,2}
+    m:set_speed(40)
+    m:set_speed(20)
+    m:set_ignore_obstacles(true)
+    m:start(pirate_guard, function()
+--      sol.timer.start(map, 1000, function() pirate_guard:set_enabled(false) end)
+      pirate_guard:set_enabled(false)
+      game:set_value("yarrow_village_pirate_guard_left", true)
+    end)
+
+  else --if you haven't already talked to Nina
+    game:start_dialog("_yarrowmouth.npcs.guard_pirate.1")
+  end
+end
+
+
+function nina:on_interaction()
+  if game:get_value("nina_dialog_counter") == 2 then
+    game:start_dialog("_yarrowmouth.npcs.nina.marsh1", function()
+      game:set_value("nina_dialog_counter", 3)
+      game:set_value("quest_yarrow_parley", 3) --quest log
+    end)
+
+  elseif game:get_value("nina_dialog_counter") == 3 then
+    game:start_dialog("_yarrowmouth.npcs.nina.6")
+  end
+
+end
--- data/maps/Yarrowmouth/yarrowmouth_village.lua
+++ data/maps/Yarrowmouth/yarrowmouth_village.lua
@@ -9,103 +9,16 @@
 
 local map = ...
 local game = map:get_game()
-local shop = require("scripts/shops/blacksmith")
 
+-- Event called at initialization time, as soon as this map is loaded.
+function map:on_started()
 
--- Event called at initialization time, as soon as this map becomes is loaded.
-map:register_event("on_started", function()
-  goatshead_teletransport:set_enabled(false)
-  oakhaven_teletransport:set_enabled(false)
-  to_limestone:set_enabled(false)
-  if game:get_value("dream_cannons_defeated") == true then carlov:set_enabled(false) end
-  if game:get_value("yarrowmouth_bird_temple_door_opened") == true then bird_repaired:set_enabled(true) end
-
-  local gm=sol.movement.create("random")
-  gm:set_speed(10)
-  gm:start(goat)
-
-end)
-
-
-function goatshead_ferry:on_interaction()
-  game:start_dialog("_ferries.goatshead", function(answer)
-    if answer == 3 then
-      if game:get_money() >9 then
-        game:remove_money(10)
-        hero:teleport("goatshead_island/goatshead_harbor", "ferry_landing")
-      else
-        game:start_dialog("_game.insufficient_funds")
-      end
-    end
-  end)
+  -- You can initialize the movement and sprites of various
+  -- map entities here.
 end
 
-function oakhaven_ferry:on_interaction()
-  game:start_dialog("_ferries.oakhaven", function(answer)
-    if answer == 3 then
-      if game:get_money() >9 then
-        game:remove_money(10)
-        hero:teleport("oakhaven/port", "from_ferry")
-      else
-        game:start_dialog("_game.insufficient_funds")
-      end
-    end
-  end)
+-- Event called after the opening transition effect of the map,
+-- that is, when the player takes control of the hero.
+function map:on_opening_transition_finished()
+
 end
-
-function broken_bird_statue:on_interaction()
-  if game:has_item("stone_beak") == true then
-    game:start_dialog("_yarrowmouth.observations.broken_bird.2", function(answer)
-      if answer == 3 then
-        game:set_value("quest_stone_beak", 1)
-        bird_repaired:set_enabled(true)
-        map:open_doors("bird_beak_door")
-        sol.audio.play_sound("secret")
-        game:set_value("yarrowmouth_bird_temple_door_opened", true)
-      end
-    end)
-  else
-    game:start_dialog("_yarrowmouth.observations.broken_bird.1")
-  end
-end
-
-
---armorer pinecone quest
-function mera:on_interaction()
-  if game:get_value("yarrow_mera_armor_obtained") ~= true then --if you don't already have the armor
-  --if you have a pinecone
-    if game:has_item("iron_pinecone") == true then
-      game:start_dialog("_yarrowmouth.npcs.mera.3", function(answer)
-
-        if answer == 2 then
-          if game:get_money() >49 then
-            game:remove_money(50)
-            game:start_dialog("_yarrowmouth.npcs.mera.4", function() game:set_value("quest_iron_pine_cone", 3) end) --ql
-            game:set_value("defense", game:get_value("defense") +2)
-            game:set_value("yarrow_mera_armor_obtained", true)
-
-          else
-            game:start_dialog("_game.insufficient_funds")
-            if game:get_value("quest_iron_pine_cone") == 1 then
-              game:set_value("quest_iron_pine_cone", 2) --ql
-            end
-          end --end money check
-        end --end of answer check
-
-      end) --end of mera.3 dialog
-
-    else --if you don't have the pinecone yet
-      game:start_dialog("_yarrowmouth.npcs.mera.1", function(answer)
-        game:start_dialog("_yarrowmouth.npcs.mera.2", function() game:set_value("quest_iron_pine_cone", 0) end) --ql
-      end) --end of how you answer if you want armor
-    end--end of if you have pinecone or not
-  else --if you have already gotten the armor
-    game:start_dialog("_yarrowmouth.npcs.mera.5")
-  end
-end
-
-
-
-function blacksmith:on_interaction()
-  shop:open_shop(game)
-end --blacksmith interaction end
