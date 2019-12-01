0f0c2112eb83c16f588f391437e011e2818fbfd6
maxmraz <max.mraz.11@gmail.com>
2019-12-01 01:06:17 -0500

    connect silent glade

--- data/maps/Yarrowmouth/puzzlewood.lua
+++ data/maps/Yarrowmouth/puzzlewood.lua
@@ -1,3 +1,19 @@
 local map = ...
 local game = map:get_game()
 
+map:register_event("on_started", function()
+  require("scripts/fx/sound_atmosphere_manager"):start_atmosphere(map, "birds")
+
+--footprints
+  if game:get_value("puzzlewood_footprints_visible") == true then
+    for prints in map:get_entities("footprints") do
+      prints:set_enabled(true)
+    end
+  else
+    for prints in map:get_entities("footprints") do
+      prints:set_enabled(false)
+    end
+  end
+  
+
+end)
