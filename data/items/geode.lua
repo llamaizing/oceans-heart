cdb9f7bd8dc2ac4a57019dd5a079675cc7698695
maxmraz <max.mraz.11@gmail.com>
2019-12-11 00:08:58 -0500

    add sculptor monsters fight

--- data/items/geode.lua
+++ data/items/geode.lua
@@ -11,7 +11,7 @@ end)
 
 item:register_event("on_obtaining", function(self, variant, savegame_variable)
   if game:has_item(item:get_name()) then item:set_brandish_when_picked(false) end
-  local amounts = { 1, 5, 10 }
+  local amounts = { 1, 5, 10, 20}
   local amount = amounts[variant]
   if amount == nil then
     error("Invalid variant '" .. variant .. "' for item 'geode'")
