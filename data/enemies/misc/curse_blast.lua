e8456ba7735293d1366ea71baa9471ff58c72cc1
maxmraz <max.mraz.11@gmail.com>
2019-12-21 00:28:08 -0500

    design abyss beast

---
+++ data/enemies/misc/curse_blast.lua
@@ -0,0 +1,26 @@
+local enemy = ...
+local game = enemy:get_game()
+local map = enemy:get_map()
+local hero = map:get_hero()
+local sprite
+local movement
+enemy.immobilize_immunity = true
+enemy.lighting_effect = 2
+
+function enemy:on_created()
+  sprite = enemy:create_sprite("enemies/" .. enemy:get_breed())
+  enemy:set_life(1)
+  enemy:set_damage(14)
+  enemy:set_can_attack(false)
+  enemy:set_invincible(true)
+end
+
+function enemy:on_restarted()
+--  if enemy:get_distance(hero) < 400 and enemy:is_in_same_region(hero) then sol.audio.play_sound("fire_burst_2") end
+  sprite:set_animation("charging", function()
+    enemy:set_can_attack(true)
+    sprite:set_animation("burning", function()
+      enemy:remove()
+    end)
+  end)
+end
---
+++ data/enemies/misc/curse_skull.lua
@@ -0,0 +1,80 @@
+local enemy = ...
+local bounces = 0
+local MAX_BOUNCES = 0
+local FUSE_LENGTH = 2000
+enemy.immobilize_immunity = true
+enemy.lighting_effect = 1
+
+function enemy:on_created()
+  sprite = enemy:create_sprite("enemies/".. enemy:get_breed())
+  enemy:set_life(1)
+  enemy:set_damage(8)
+  enemy:set_origin(4, 4)
+  enemy:set_obstacle_behavior("flying")
+  enemy:set_layer_independent_collisions(true)
+  enemy:set_can_hurt_hero_running(true)
+--  enemy:set_invincible()
+  enemy:set_attack_consequence("sword", "custom")
+  enemy:set_attack_consequence("arrow", "ignored")
+  bounces = 0
+  enemy:set_dying_sprite_id("enemies/enemy_killed_small")
+end
+
+function enemy:set_max_bounces(amount)
+  MAX_BOUNCES = amount
+end
+
+function enemy:go(direction)
+  local movement = sol.movement.create("straight")
+  movement:set_speed(100)
+  movement:set_angle(direction)
+  movement:set_smooth(false)
+  movement:start(enemy)
+
+  function movement:on_obstacle_reached()
+    if bounces < MAX_BOUNCES then
+      bounces = bounces + 1
+      enemy:go(enemy:get_new_direction())
+    else
+      enemy:remove()
+    end
+  end
+
+  sol.timer.start(enemy, FUSE_LENGTH, function() enemy:remove() end)
+end
+
+
+-- Destroy the fireball when the hero is touched.
+function enemy:on_attacking_hero(hero, enemy_sprite)
+  hero:start_hurt(enemy, enemy_sprite, enemy:get_damage())
+  enemy:remove()
+end
+
+-- Change the direction of the movement when hit with the sword.
+function enemy:on_custom_attack_received(attack, sprite)
+  if attack == "sword" then
+    sol.audio.play_sound("enemy_hurt")
+    enemy:go(enemy:get_angle(enemy:get_map():get_hero()) + math.pi)
+  end
+end
+
+
+
+--Calculate New Direction
+function enemy:get_new_direction()
+  local wall_orientation = enemy:get_collision_wall_orientation()
+  local current_angle = enemy:get_movement():get_angle()
+  local new_angle
+  if wall_orientation == "vertical" then
+    new_angle = math.pi - current_angle
+  else
+    new_angle = 2*math.pi - current_angle
+  end  
+  return new_angle
+end
+
+--Get Wall Horiz or Vert
+function enemy:get_collision_wall_orientation()
+  if enemy:test_obstacles(8, 0) or enemy:test_obstacles(-8, 0) then return "vertical" end
+  if enemy:test_obstacles(0, 8) or enemy:test_obstacles(0, -8) then return "hoirzontal" end
+end
