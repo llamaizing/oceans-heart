bbee6bc2c2aa84b260d2bb8b6f0dca9b4644ffda
maxmraz <max.mraz.11@gmail.com>
2019-12-28 18:05:18 -0500

    typo in dream cannon hideout, add dialog to books in pirate vault

--- data/maps/ballast_harbor/dream_cannon_hideout.lua
+++ data/maps/ballast_harbor/dream_cannon_hideout.lua
@@ -11,7 +11,7 @@ local map = ...
 local game = map:get_game()
 
 
-map:register_event("on_started" function()
+map:register_event("on_started", function()
   local lighting_effects = require"scripts/fx/lighting_effects"
   lighting_effects:initialize()
   lighting_effects:set_darkness_level(1)
