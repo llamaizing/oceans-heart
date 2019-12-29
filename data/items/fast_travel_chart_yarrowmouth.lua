cbd34b3ac7205d39b9efea9dc0adc97e3ccf0a03
maxmraz <max.mraz.11@gmail.com>
2019-12-28 19:30:18 -0500

    fix yarrowmouth chart not adding it to map

--- data/items/fast_travel_chart_yarrowmouth.lua
+++ data/items/fast_travel_chart_yarrowmouth.lua
@@ -5,3 +5,6 @@ item:register_event("on_created", function(self)
   item:set_savegame_variable("fast_travel_chart_yarrowmouth")
 end)
 
+item:register_event("on_obtained", function(self)
+  game.world_map:set_map_visible("Yarrowmouth/yarrowmouth_village")
+end)
